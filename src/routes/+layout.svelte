f48ac99a19aec8b0e28e0439891353fcbec77fb4
reubenjds <reubenjds@gmail.com>
2023-09-24 15:45:37 -0400

    feat: added CoinFlip logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 <div class="navbar bg-base-100">
   <div class="navbar-start">
-    <a href="/" class="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <a href="/" class="btn btn-ghost normal-case text-xl"><span class="text-2xl font-bold">Coin<span class="text-2xl font-bold text-yellow-400">Flip</span></span></a>
   </div>
   <div class="navbar-end">
     <a href="/test" class="btn">Leaderboard</a>
