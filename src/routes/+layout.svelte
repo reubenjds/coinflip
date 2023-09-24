9d0e7a9f0568a5cf417f661af1253556eab90210
reubenjds <reubenjds@gmail.com>
2023-09-23 20:06:54 -0400

    feat: added heads and tails dropdown and implemented flip button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
-  import "../app.css";
+  import "../app.css";  
 </script>
 
+<div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <a href="/" class="btn btn-ghost normal-case text-xl">daisyUI</a>
+  </div>
+  <div class="navbar-end">
+    <a href="/test" class="btn">Leaderboard</a>
+  </div>
+</div>
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,37 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-lg">
-  Visit <a class="underline text-primary-content bg-primary" href="/test"
-    >kit.svelte.dev</a
-  > to read the documentation
-</p>
-<button class="btn btn-success">Click</button>
+<script lang="ts">
+	import { goto } from "$app/navigation";
+
+
+  function getRandomInt(max:number) {
+    return Math.floor(Math.random() * max);
+  }
+
+  let selectedFlip: number;
+  let flip = -1;
+  let streak = 0;
+  function flipCoin() {
+    flip = getRandomInt(2);
+    streak++;
+  }
+
+</script>
+
+<div class="flex flex-col text-lg justify-center place-items-center min-h-screen gap-5"> 
+  {#if flip === -1}
+  <select bind:value={selectedFlip} class="select select-secondary w-full max-w-xs">
+    <option value={1} selected>heads</option>
+    <option value={0}>tails</option>
+  </select>
+  {:else if flip === 1}
+      <h2 class="text-orange-600" >heads</h2>
+  {:else} 
+      <h2 class="text-cyan-400">tails</h2>
+  {/if}
+
+<button disabled={selectedFlip !== flip && flip !== -1} class="btn btn-success" on:click={flipCoin}>Flip Coin</button>
+{#if streak !== 0}
+<h1> Your streak is {streak}</h1>
+{/if}
+</div>
+
+
