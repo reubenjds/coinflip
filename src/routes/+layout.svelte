ad8bfdd9a7abc991b38f6ce22975fd2a4a9392a6
reubenjds <reubenjds@gmail.com>
2023-09-27 20:05:50 -0400

    feat: changed leaderboard to correct route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,7 +7,7 @@
     <a href="/" class="btn btn-ghost normal-case text-xl"><span class="text-2xl font-bold">Coin<span class="text-2xl font-bold text-yellow-400">Flip</span></span></a>
   </div>
   <div class="navbar-end">
-    <a href="/test" class="btn">Leaderboard</a>
+    <a href="/leaderboard" class="btn">Leaderboard</a>
   </div>
 </div>
 <slot />
